3399d21378ddfcd9a5ca2b5dd222ab62188109d0
khushbu <139963700+khushbu1224@users.noreply.github.com>
2024-07-24 11:20:21 +0000

    const  let & var

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,20 @@
+const accountId = 1224
+let accountEmail ="khushbu@1786"
+var accountPassword ="122400"
+accountcity = "Delhi"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail ="shere@1786"
+accountPassword =1985
+accountcity = "mumbai"
+
+console.log("accountId")
+/*
+prefer not used var
+because of issue in block scope and functional scope
+
+*/
+ 
+console.table([accountId,accountEmail,accountPassword,accountcity, accountState])
